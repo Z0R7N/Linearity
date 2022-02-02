7d90719c89c42d5893d5b3ef914530070358f094
Z0R7N <46117076+Z0R7N@users.noreply.github.com>
2022-02-02 13:56:25 +0300

    add wolring for table part

--- applications/VBA/variables.vb
+++ applications/VBA/variables.vb
@@ -4,16 +4,16 @@ Dim ports() As String
 Dim lBoundVar As Long
 Dim uBoundVar As Long
 'Public workingPort As String
-Dim numPort As Long
-Dim passControl As Double              ' password for check port control
+'Dim numPort As Long
 Public portControl As String           ' portControl in range AA2
 Public portMultimetr As String         ' portMultimetr in range AA1
 Public work1 As Boolean
 Public work2 As Boolean
+Dim work As Boolean                 ' block buttons
 Dim checkControl As Boolean         ' if port of control rotation is correct
 Dim checkMultimetr As Boolean       ' if port of multimetr is correct
 Dim sendMulty As String
 Public cellPort1, cellPort2 As String   ' keeping data serials (in sub setParameter)
 Dim cellLog As Long                  ' cells for loging
-Dim needLog As Boolean               ' if log is need - true
+Dim needLog As Boolean 
 
